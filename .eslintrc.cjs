42b63d92b6d2ba368820f32db4f397bd98581f60
Brendan <b.campbell787@gmail.com>
2024-08-09 18:59:10 +0100

    Vercel ignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ const config = {
         "@typescript-eslint/no-unsafe-argument": "warn",
         "@typescript-eslint/no-unsafe-assignment": "warn",
         "@typescript-eslint/no-unsafe-return": "warn",
+        "@typescript-eslint/ban-ts-comment": "warn",
         "@typescript-eslint/consistent-type-imports": [
             "warn",
             {
